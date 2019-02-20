6b7be6a42ee535abeda423c3b8c2fbcd7e812781
Adam C. Foltzer <acfoltzer@acfoltzer.net>
2019-02-20 12:32:15 -0800

    check in example of cbindgen not finding struct in dependency

---
+++ build.rs
@@ -0,0 +1,15 @@
+use cbindgen;
+use std::env;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .with_language(cbindgen::Language::C)
+        .with_parse_deps(true)
+        .with_parse_include(&["dep"])
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file("bindings.h");
+}
