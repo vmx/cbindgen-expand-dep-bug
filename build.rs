c50d6f244b5af0c6da4f8b57ee318a7ee56ed31f
Volker Mische <volker.mische@gmail.com>
2019-08-17 15:55:07 +0200

    Use cbindgen.toml instead of build.rs configuration

--- build.rs
+++ build.rs
@@ -4,12 +4,7 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    cbindgen::Builder::new()
-        .with_crate(crate_dir)
-        .with_language(cbindgen::Language::C)
-        .with_parse_deps(true)
-        .with_parse_include(&["dep"])
-        .generate()
+    cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
         .write_to_file("bindings.h");
 }
